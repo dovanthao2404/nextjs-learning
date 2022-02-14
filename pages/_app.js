717efbe259f19b85ffcdae4acda1f4712e4adf43
dovanthao2404 <dovanthao2404@gmail.com>
2022-02-14 16:50:05 +0700

    redux config with next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { Provider } from 'react-redux';
+import '../styles/globals.css';
+import store from '../redux/store';
+import { createWrapper } from "next-redux-wrapper";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, ...props }) {
+  return <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>;
 }
 
-export default MyApp
+
+const makeStore = () => store;
+
+const wrapper = createWrapper(makeStore);
+
+
+export default wrapper.withRedux(MyApp);
