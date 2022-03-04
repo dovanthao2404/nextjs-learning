f644ba5f7f6e2a498c5d2af43b32be8a47f8879c
dovanthao2404 <dovanthao2404@gmail.com>
2022-03-04 16:09:33 +0700

    update

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -2,30 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '../../components/layout';
 
-function Detail() {
-    const [movie, setMovie] = useState();
+function Detail(props) {
+    // const [movie, setMovie] = useState();
     const [isLoading, setIsLoading] = useState(false);
-    const router = useRouter();
 
-    useEffect(() => {
-        if (Object.keys(router.query).length > 0) {
-            ; (async () => {
-                setIsLoading(true);
-                const res = await fetch(`https://json-serve-hihi.herokuapp.com/api/v1/movies/${router.query?.id}`);
-                const data = await res.json();
-                setMovie(data);
-                setIsLoading(false);
-            })();
-        }
-    }, [router]);
-    if (isLoading) return <>Loading....</>;
-
+    console.log(props);
     return (
         <Layout meta={
             <>
                 <title>Booking Movie</title>
                 <meta name="description" content="Booking movie" />
-                <meta property="og:image" content={movie?.poster} />
+                <meta property="og:image" content={props.data?.poster} />
             </>
         }>
             <div>[id]</div>
@@ -33,27 +20,15 @@ function Detail() {
     );
 }
 
+export async function getServerSideProps(context) {
+    const { id } = context.query;
 
-// // pages/blog/[slug].js
-// export async function getStaticPaths(a) {
-//     console.log(a);
-//     return {
-//         paths: [
-//             // String variant:
-//             '/movie/2',
-//             // Object variant:
-//             { params: { id: '2' } },
-//         ],
-//         fallback: true,
-//     };
-// }
-
-// export async function getStaticProps(context) {
-//     console.log(context);
-//     return {
-//         props: {}, // will be passed to the page component as props
-//     };
-// }
+    const res = await fetch(`https://json-serve-hihi.herokuapp.com/api/v1/movies/${id}`);
+    const data = await res.json();
+    return {
+        props: { data } // will be passed to the page component as props
+    };
+}
 
 
 export default Detail;
